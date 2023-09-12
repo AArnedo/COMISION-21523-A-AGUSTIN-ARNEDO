83550718e55f5a62f4b361ee643fda1caf030a70
AArnedo <arnedoagustin@gmail.com>
2023-09-12 13:48:00 -0300

    Agregando mas dependecias y archivo gitignore para omitir los archivos subidos en la carpeta node_modules

--- app.js
+++ app.js
@@ -2,16 +2,30 @@
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const ejs = require('ejs');
+const dotenv = require('dotenv');
 const app = express()
 const port = 4000;
 
 /* Middlewares */
 app.use(cors())
-app.use(morgan('dev'))
+app.use(morgan('combined'))
+app.use(express.json()) /* Se utiliza para que el servidor pueda comprender el formato JSON */
+
 
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.post('/user' , function (req, res) {
+
+    /* Recibimos los datos por el body */
+    const { name, lastname } = req.body
+
+    res.send(`¡Bienvenido ${name} ${lastname}!`)
+
 })
 
 app.listen(port, () => console.log(`Servidor en http://localhost:${port}`))
